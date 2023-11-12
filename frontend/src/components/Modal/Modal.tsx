e369ed68517f4ae2b735b5b054e6c37a96cb753d
CharelSuarez <114007611+CharelSuarez@users.noreply.github.com>
2023-11-11 22:03:56 -0500

    Created login and register modal components

---
+++ frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,17 @@
+import './Modal.css';
+
+interface ModalProps {
+    onClose: () => void;
+    children: React.ReactNode;
+}
+
+export default function Modal({ onClose, children }: ModalProps) {
+    return (
+        <>
+            <div className='modal'>
+                <img className='close-button' src='https://i.imgur.com/O3YBoxX.png' alt='close' onClick={onClose} />
+                {children}
+            </div>
+        </>
+    );
+}
---
+++ frontend/src/components/Taskbar/Taskbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from 'next/link';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import LoginModal from '@/components/LoginModal/LoginModal';
+import './Taskbar.css';
+import RegisterModal from '../RegisterModal/RegisterModal';
+
+export default function Taskbar(){
+    const [showLoginModal, setLoginShowModal] = useState(false);
+    const [showRegisterModal, setShowRegisterModal] = useState(false);
+
+    return (
+        <div className='taskbar'>
+            <h1>joe's website</h1>
+            <div className="links">
+                <Link href="/dashboard">
+                    <button>Dashboard</button>
+                </Link>
+                <Link href="/map">
+                    <button>Map</button>
+                </Link>
+                <Link href="/social">
+                    <button>Social</button>
+                </Link>
+                <button onClick={() => setLoginShowModal(true)}>
+                Login
+                </button>
+                {showLoginModal && createPortal(
+                    <LoginModal onClose={() => setLoginShowModal(false)} />,
+                    document.body
+                )}
+                <button onClick={() => setShowRegisterModal(true)}>
+                Register
+                </button>
+                {showRegisterModal && createPortal(
+                    <RegisterModal onClose={() => setShowRegisterModal(false)} />,
+                    document.body
+                )}
+            </div>
+        </div>
+    )
+}
