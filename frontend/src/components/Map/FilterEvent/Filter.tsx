16981fcd993e3198c27f94b9f72870a043bfa57d
ashvin <a.sureskumar@mail.utoronto.ca>
2023-11-11 22:33:54 -0500

    Added Filter

--- frontend/src/components/Map/FilterEvent/Filter.tsx
+++ frontend/src/components/Map/FilterEvent/Filter.tsx
@@ -4,7 +4,29 @@ export default function Filter(){
     return (
       <div className="filterBox">
         <div className="filters">
-            
+
+          <div className="oneFilter">
+            <label>Start Date:</label>
+            <input type="date"></input>
+          </div>
+
+          <div className="oneFilter">
+            <label>End Date:</label>
+            <input type="date"></input>
+          </div>
+
+          <div className="oneFilter">
+            <label>Location:</label>
+            <select name="locations" id="locations">
+              <option value="SW">SW</option>
+              <option value="MW">MW</option>
+              <option value="IC">IC</option>
+              <option value="AC">AC</option>
+            </select>
+          </div>
+
+          <button></button>
+          
         </div>
         <Events></Events>
       </div>
